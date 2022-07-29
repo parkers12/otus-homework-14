fdab5cbba4ea0524bfb08d76d619b394ceb91b3d
PARKER\Parker <parkers@mail.ru>
2022-07-29 14:51:34 +0300

    configureStore

---
+++ src/configureStore.ts
@@ -0,0 +1,45 @@
+export type Store<State = any, Action = { type: string }> = {
+  getState(): State;
+  dispatch(action: Action): any;
+  subscribe(cb: () => void): () => void;
+};
+
+export type Reducer<State, Action> = (
+  state: State | undefined,
+  action: Action
+) => State;
+
+export type Middleware<State, Action> = (
+  store: Store<State, Action>
+) => (next: (action: Action) => any) => (action: Action) => any;
+
+export type ConfigureStore<State, Action> = (
+  reducer: Reducer<State, Action>,
+  initialState?: State | undefined,
+  middlewares?: Middleware<State, Action>[]
+) => Store<State, Action>;
+
+export function configureStore<State, Action>(
+  reducer: Reducer<State, Action>,
+  initialState?: State | undefined,
+  middlewares?: Middleware<State, Action>[]
+): Store<State, Action> {
+  let state: State = initialState;
+  let subscribers: [] = [];
+
+  return {
+    getState(): State {
+      return state;
+    },
+    dispatch(action: Action): any {
+      state = reducer(state, action);
+      subscribers.forEach((cb) => cb());
+    },
+    subscribe(cb: () => void): () => void {
+      subscribers.push(cb);
+      return () => {
+        subscribers = subscribers.filter((el) => el !== cb);
+      };
+    }
+  };
+}
